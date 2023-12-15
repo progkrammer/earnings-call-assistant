37f6d1d621b279dceb6257682cbc3d0294e7a5e8
progkrammer <progkrammer@gmail.com>
2023-12-15 06:28:55 -0600

    added loading wheel and updated summary prompt to be more clear.

--- app.py
+++ app.py
@@ -50,29 +50,34 @@ if symbol:
         use_container_width=True
     )
     if st.session_state.clicked:
-        url_transcription = f"https://financialmodelingprep.com/api/v3/earning_call_transcript/{symbol}?apikey={fmp_api_key}"
-        response_transcription = requests.get(url_transcription)
-        transcript = response_transcription.json()[0]
-        content = transcript["content"]
-        symbol = transcript["symbol"]
-        year = transcript["year"]
-        quarter = transcript["quarter"]
-        try:
-            response_gemini = model.generate_content([
-                "You are a financial analyst reviewing earnings call transcripts. ",
-                f"Your job is to provide a Markdown-formatted summary of the following transcript for {symbol}. ",
-                f"Begin the summary with the Ticker ({symbol}), Year ({year}), and Quarter {quarter}. ",
-                f"You should be looking for insights related to the financial performance of {symbol}. ",
-                "Additionally, you should highlight management's commentary in terms of confidence and optimism, as well as risk factors for future growth. ",
-                "Finally, you should provide a summary of the investment implications in the short term and long term. ",
-                "Remember to format your response as Markdown and should read as an esay-to-digest financial article. ",
-                content
-            ]).text
-        except Exception as e:
-            response_gemini = f"Sorry, there was an error in processing your request. This happens sometimes. Please reset and try again :) \n\n {e}"
+        with st.spinner(text="In progress..."):
+            url_transcription = f"https://financialmodelingprep.com/api/v3/earning_call_transcript/{symbol}?apikey={fmp_api_key}"
+            response_transcription = requests.get(url_transcription)
+            transcript = response_transcription.json()[0]
+            content = transcript["content"]
+            symbol = transcript["symbol"]
+            year = transcript["year"]
+            quarter = transcript["quarter"]
+            try:
+                response_gemini = model.generate_content([
+                    "You are a financial analyst reviewing earnings call transcripts. ",
+                    f"Your job is to provide a Markdown-formatted summary of the following transcript for {symbol}. ",
+                    f"Begin the summary with a header containing Ticker ({symbol}), Year ({year}), and Quarter ({quarter}). ",
+                    f"For example: {year} Q{quarter} Earnings Call Summary for {symbol}. ",
+                    "The next section should have the header Key Takeaways, which summarizes the key points for an investor in 5-7 bullet points at maximum. Don't exceed 7 bullet points. ",
+                    "After that, you should have a section with a header Financial Results, which highlights the key financial takeaways. ",
+                    "Then, the next section should have the header Analysis and Commentary, which highlights the key reflections from management. ",
+                    f"The next section should have the header Risk Factors, which details the main risk factors for the future growth of {symbol}. ",
+                    "Finally, the last section should have the header Investment Implications, with subsections highlighting the short-term (sub-header Short-term) and long-term (sub-header Long-term) investment implications, respectively. ",
+                    "Remember to format your response as Markdown and should read as an esay-to-digest financial article. Make sure to use the proper headers with the defined content.",
+                    content
+                ]).text
+            except Exception as e:
+                response_gemini = f"Sorry, there was an error in processing your request. This happens sometimes. Please reset and try again :) \n\n {e}"
         st.markdown(response_gemini.replace("$", "\$"))
         st.button(
             label="Reset",
+            type="primary",
             on_click=reset_states,
             use_container_width=True
         )
